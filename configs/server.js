31f67f91b840309eda241609e64b99382ae46412
Informatica <informatica@alumno.local>
2025-02-26 14:20:58 -0600

    usario por default y login :D

--- configs/server.js
+++ configs/server.js
@@ -5,6 +5,12 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { dbConnection } from './mongo.js';
+import limiter from '../src/middlewares/validar-cant-peticiones.js'
+import authRoutes from '../src/auth/auth.routes.js'
+import authUsers from '../src/users/user.routes.js'
+
+import User from '../src/users/user.model.js'
+import { hash } from "argon2";
 
 const configurarMiddlewares = (app) => {
     app.use(express.urlencoded({extended: false}));
@@ -12,6 +18,12 @@ const configurarMiddlewares = (app) => {
     app.use(express.json());
     app.use(helmet());
     app.use(morgan('dev'));
+    app.use(limiter);
+}
+
+const configurarRutas = (app) =>{
+    app.use("/Empresa/v1/auth", authRoutes);
+    app.use("/Empresa/v1/users",authUsers);
 }
 
 const conectarDB = async () => {
@@ -23,12 +35,40 @@ const conectarDB = async () => {
     }
 }
 
+const crearAdmin = async () => {
+    try {
+        const adminastradorExistente = await User.findOne({ role: "ADMIN_ROLE" });
+
+        if (!adminastradorExistente) {
+            const passwordEncriptada = await hash("Admin123");
+
+            const admin = new User({
+                name: "Admin",
+                username: "admin",
+                email: "admin@gmail.com",
+                phone: "123456789",
+                password: passwordEncriptada,
+                role: "ADMIN_ROLE"
+            });
+
+            await admin.save();
+            console.log("Administrador creado exitosamente.:p");
+        } else {
+            console.log("El administrador ya existe.:P");
+        }
+    } catch (error) {
+        console.error("Error al crear el administrador:", error);
+    }
+};
+
 export const iniciarServidor = async () => {
     const app = express();
     const port = process.env.PORT || 3010;
 
     await conectarDB();
+    await crearAdmin();
     configurarMiddlewares(app);
+    configurarRutas(app);
 
     app.listen(port, () => {
         console.log(`Server Running On Port ${port}`);
